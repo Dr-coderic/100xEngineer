ba6e3abc88935bc828f2059ef366f988fdfa18e1
Dr-coderic <qdrqvaibhav2@gmail.com>
2026-09-22 00:45:48 +0530

    java basics, string , concatenation, sout, comments

--- Java Refresher/src/main/java/A_HelloWorld.java
+++ Java Refresher/src/main/java/A_HelloWorld.java
@@ -1,5 +1,51 @@
 public class A_HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        String name = "Vaibhav";
+
+        // System.out.print()
+        System.out.print("Hello World");
+//        system.out.print("Hello World");    --> compilation error: java is case-sensitive
+        System.out.print("My name is: " + name );
+
+        System.out.println();
+        //System.out.println()
+        System.out.println("Hello world");
+        System.out.println("My name is: " + name);
+
+        // --> single line of comment
+
+        /*
+        * multi line comment
+        *
+        */
+
+        //printing numbers
+        System.out.println(10);
+        System.out.println(10+5);   // when an operator is placed in b/w numbers, first the value is calculated then the result is printed.
+        System.out.println(10-5);   //5
+        System.out.println(10*5);   //50
+        System.out.println(10/5);   //2
+        System.out.println(-10);
+        System.out.println(11.268);
+
+//        Quiz
+        System.out.println(8-5);    //  3
+        System.out.println(10);     //  10
+        System.out.println("10");   //  10
+        System.out.println("5*3");  //  5*3
+        System.out.println(5*3);    //  15
+
+
+        //Concatenation
+        System.out.println("Ram" + " aur" + " Lakhan"); //first concat - Ram aur Lakhan --> then print
+        System.out.println("My age is " + 25);          // My age is 25
+        System.out.println("Random " + 10 + 30);        //Random 1030
+        // ^ reason, addition happens left to right, -->"Random "+ 10 + 30 --> "Ransom 10" + 30 --> "Random 1030"
+        System.out.print(10 + 30 + " Random");          // 40 Random
+
+
+
+
     }
 }
